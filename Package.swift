f6487f1b403b07eda8b3eb024437c8b6030e2161
asadkhan <asad@figgers.com>
2025-06-23 15:10:41 +0500

    new

--- Package.swift
+++ Package.swift
@@ -1,48 +1,16 @@
-
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
-    name: "JSONModel",
-    platforms: [
-        .macOS(.v10_10),
-        .iOS(.v9),
-        .tvOS(.v9),
-        .watchOS(.v2)
-    ],
-    products: [
-        .library(name: "JSONModel",
-                 targets: ["JSONModel", "JSONModelNetworking" , "JSONModelTransformations"])
-    ],
-    targets: [
-        .target(
-            name: "JSONModel",
-            dependencies: [
-                "JSONModelTransformations"
-            ],
-            path: "JSONModel/JSONModel",
-            sources: [
-                ""
-            ],
-            publicHeadersPath: ""
-        ),
-        .target(
-            name: "JSONModelNetworking",
-            dependencies: [
-                "JSONModel"
-            ],
-            path: "JSONModel/JSONModelNetworking",
-            sources: [
-                ""
-            ],
-            publicHeadersPath: ""
-        ),
-        .target(
-            name: "JSONModelTransformations",
-            path: "JSONModel/JSONModelTransformations",
-            sources: [
-                ""
-            ],
-            publicHeadersPath: ""
-        )
-    ]
+  name: "JSONModel",
+  platforms: [.iOS(.v9)],
+  products: [
+    .library(name: "JSONModel", targets: ["JSONModel"]),
+  ],
+  targets: [
+    .target(
+      name: "JSONModel",
+      path: "JSONModel"       // ya jahan headers & sources hain
+    ),
+  ]
 )
